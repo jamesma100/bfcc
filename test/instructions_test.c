dcc388fd7d61918bba2c0c4882191d1a885fd978
James Ma <jamesma100@gmail.com>
2024-02-25 18:00:00 -0800

    kill bad tests

--- test/instructions_test.c
+++ test/instructions_test.c
@@ -28,27 +28,6 @@ int main(void) {
   //     |
   assert(out(&mem) == 2);
 
-  int num_rows = 100;
-  int num_cols = 100;
-
-  Instructions instructions;
-  init_instructions(&instructions, num_rows, num_cols);
-
-  jmp_if_nonzero(&mem, &instructions, 2, 7);
-  assert(
-    instructions.linenum == 2 &&
-    instructions.pos == 7
-  );
-  right(&mem);
-  // 1 0 2 0 0 0 0 0 0 0
-  //       |
-  jmp_if_zero(&mem, &instructions, 6, 9);
-  assert(
-    instructions.linenum == 6 &&
-    instructions.pos == 9
-  );
-
-  free_instructions(&instructions);
   free_memory(&mem);
 
   printf("[TEST] all instructions tests passed!\n");
